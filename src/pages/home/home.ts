a40bb1475a12dc303531617eed8993f9a0e075f5
shane98c <shane98c@gmail.com>
2017-03-22 16:53:25 -0500

    basic marker example working, no animation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,39 @@ import { UnderService } from '../../shared/under.service'
 import { LocService } from '../../shared/loc.service'
 import { layers } from '../../shared/layers'
 
+let geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "message": "Foo",
+                "iconSize": [60, 60]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -66.324462890625,
+                    -16.024695711685304
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "message": "Bar",
+                "iconSize": [50, 50]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -61.2158203125,
+                    -15.97189158092897
+                ]
+            }
+        }
+    ]
+};
 const accessToken = 'pk.eyJ1IjoiZmx5b3ZlcmNvdW50cnkiLCJhIjoiNDI2NzYzMmYxMzI5NWYxMDc0YTY5NzRiMzdlZDIyNTAifQ.x4T-qLEzRQMNFIdnkOkHKQ';
 
 @Component({
@@ -21,71 +54,36 @@ export class HomePage {
   ngOnInit(): void {
     this.mapCtrl();
   }
+
   mapCtrl(): void {
-    let bounds:number[][]  = [
-      [-97.53662, 42.994854], // Southwest coordinates
-      [-89.49462, 49.24472443]  // Northeast coordinates
-    ]
     this.map = new mapboxgl.Map({
       center: [-94.349742, 45.98909],
-      zoom: 7,
-      maxBounds: bounds,
+      zoom: 5,
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v9'
     });
     this.map.on('load', () => {
-      this.map.addSource('mnLidar', layers.mnLidar);
-      this.map.addSource('colorTopo', layers.colorTopo);
-      this.map.addLayer({
-         'id': 'mnLidar',
-         'type': 'raster',
-         'source':"mnLidar",
-         'maxzoom': 20,
-         'minzoom': 7
-       }, 'waterway-river-canal');
-     this.map.addLayer({
-        'id': 'colorTopo',
-        'type': 'raster',
-        'source':"colorTopo",
-        'maxzoom': 20,
-        'minzoom': 7
-      }, 'waterway-river-canal');
-    this.map.setPaintProperty('colorTopo', 'raster-opacity', 0.25);
+      console.log('map load');
+    // create a DOM element for the marker
+    let el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage = 'url(https://placekitten.com/g/' + geojson.features[0].properties.iconSize.join('/') + '/)';
+    el.style.width = geojson.features[0].properties.iconSize[0] + 'px';
+    el.style.height = geojson.features[0].properties.iconSize[1] + 'px';
+
+        // add marker to map
+        new mapboxgl.Marker(el)
+            .setLngLat(geojson.features[0].geometry.coordinates)
+            .addTo(this.map);
     })
-    this.map.on('click', (e) => {
-      this.getTappedGeo(e);
-      //add marker in the future
-    });
-  }
-  getTappedGeo(e): void {
-    let loading = this.loadingCtrl.create();
-    loading.present();
-    this.underService.getUnder(e.lngLat)
-    .then(UnderData => {
-      this.renderData(UnderData);
-      loading.dismiss();
-    })
-    .catch(ex => {
-      console.error('Error getting Geology', ex);
-      loading.dismiss();
-      alert('Error finding geological information');
-    });
   }
   fabLocate(): void {
-    this.locService.getCurrentPosition()
-      .subscribe(
-        res => {
-          let center: number[] = [res.coords.longitude, res.coords.latitude]
-          this.map.flyTo({center: center, zoom: 17});
-        },
-        err => {
-          alert('Geolocation unavailable.')
-        }
-    );
-  }
-  renderData(under): void {
-    this.navCtrl.push(AboutPage, {
-      under: under
+    console.log('fab press');
+    document.getElementsByClassName('marker', (el) => {
+      let els = el;
+      els.style.width = '200 px';
+      els.style.height = '200 px';
     });
+
   }
 }
